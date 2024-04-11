9d0a9a54e84b351b5c22470bab3f5e68ccaaae7f
Fabien D <fabien.ducret@hotmail.fr>
2024-04-11 10:10:45 +0200

    [FEATURE] Change google to bing provider

--- informations/news/repository.go
+++ informations/news/repository.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"nearby/models"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const GOOGLE_URL = "https://google.com/search"
+const BING_URL = "https://www.bing.com/news/search"
 
 func NewsRepositoryFactory() func(city string) ([]models.News, error) {
 	client := http.Client{
@@ -32,41 +31,38 @@ func NewsRepositoryFactory() func(city string) ([]models.News, error) {
 		defer response.Body.Close()
 
 		document, _ := goquery.NewDocumentFromReader(response.Body)
-		elements := document.Find("div.MkXWrd")
-
-		news := []models.News{}
-
-		for index := range elements.Nodes {
-			item := elements.Eq(index)
-
-			news = append(news, parseElement(item))
-		}
+		elements := document.Find("div.news-card")
+		news := newsFromElements(elements)
 
 		return news, nil
 	}
 }
 
 func createRequest(city string) *http.Request {
-	url := fmt.Sprintf("%s?q=%s", GOOGLE_URL, city)
+	url := fmt.Sprintf("%s?q=%s", BING_URL, city)
 	request, _ := http.NewRequest(http.MethodGet, url, nil)
-	addHeadersFor(request)
 
 	return request
 }
 
-func addHeadersFor(request *http.Request) {
-	request.Header.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-	request.Header.Add("accept-language", "fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
-	request.Header.Add("cache-control", "no-cache")
-	request.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36")
+func newsFromElements(elements *goquery.Selection) []models.News {
+	news := []models.News{}
+
+	for index := range elements.Nodes {
+		item := elements.Eq(index)
+
+		news = append(news, parse(item))
+	}
+
+	return news
 }
 
-func parseElement(item *goquery.Selection) models.News {
-	link := strings.TrimSpace(item.Find("a").AttrOr("href", ""))
-	title, _ := item.Find("div.tNxQIb").Html()
+func parse(item *goquery.Selection) models.News {
+	href := item.AttrOr("data-url", "")
+	title := item.AttrOr("data-title", "")
 
 	return models.News{
-		Link:  link,
+		Link:  href,
 		Title: title,
 	}
 }
